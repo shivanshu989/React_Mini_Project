e32bf2730a27c8e5ea48d003984bd812fc83d752
sakshich27 <sakshi27ch02@gmail.com>
2023-04-24 14:28:40 +0530

    Sakshi's commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <a class="navbar-brand">Navbar</a>
     <form class="d-flex" role="search">
       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button class="btn btn-outline-success" type="submit">Search here</button>
+      <button class="btn btn-outline-success" type="submit">Search button</button>
     </form>
   </div>
 </nav>
